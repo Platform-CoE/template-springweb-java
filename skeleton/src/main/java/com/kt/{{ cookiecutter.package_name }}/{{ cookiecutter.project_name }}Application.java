af9ad1d8117a40cf7dc3db35559265229da0cc07
Jeongdong <duwjdehd0713@gmail.com>
2025-05-26 17:37:26 +0900

    feat: Update application.java

--- skeleton/src/main/java/com/kt/{{ cookiecutter.package_name }}/{{ cookiecutter.project_name }}Application.java
+++ skeleton/src/main/java/com/kt/{{ cookiecutter.package_name }}/{{ cookiecutter.project_name }}Application.java
@@ -1,7 +1,7 @@
 package com.kt.{{ cookiecutter.package_name }};
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
